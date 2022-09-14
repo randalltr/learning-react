5f6727a8378ae72c1963570b9d2fd3a95e6decf0
Randall <108246257+randalltr@users.noreply.github.com>
2022-09-14 10:57:54 +0700

    used dependency array

--- Ch04/04_01/start/react-app/src/App.js
+++ Ch04/04_01/start/react-app/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`)
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} right now`)
+  }, [secondary]);
   
   return (
     <div className="App">
@@ -12,6 +21,10 @@ function App() {
       <button onClick={() => setEmotion("happy")}>Happy</button>
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
+      <h1>Current secondary emotion is {secondary}!</h1>
+      <button onClick={() => setSecondary("tired")}>Tired</button>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
+      <button onClick={() => setSecondary("hopeful")}>Hopeful</button>
     </div>
   );
 }
