be1379e5c72c3ba4accc121be7f6d4f9251befe9
Randall <108246257+randalltr@users.noreply.github.com>
2022-09-16 12:41:31 +0700

    fetch api data with hooks

--- Ch06/06_01/start/react-app/src/App.js
+++ Ch06/06_01/start/react-app/src/App.js
@@ -1,41 +1,13 @@
 import "./App.css";
-import { useState } from "react";
-
-function useInput(initialValue) {
-  const [value, setValue] =
-    useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] =
-    useState("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(
-      `${titleProps.value}, ${colorProps.value}`
-    );
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
-      />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/randalltr`).then((response) => response.json()).then(setData);
+  }, []);
+  if (data) return (<pre>{JSON.stringify(data, null, 2)}</pre>);
+  return <h1>Data</h1>;
 }
 
 export default App;
