10d507fd5934b21fe1bcc63201215e286690d9e7
Randall <108246257+randalltr@users.noreply.github.com>
2022-09-16 12:51:29 +0700

    displaying api data

--- Ch06/06_01/start/react-app/src/App.js
+++ Ch06/06_01/start/react-app/src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function GithubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(`https://api.github.com/users/randalltr`).then((response) => response.json()).then(setData);
+    fetch(
+      `https://api.github.com/users/randalltr`
+    )
+      .then((response) => response.json())
+      .then(setData);
   }, []);
-  if (data) return (<pre>{JSON.stringify(data, null, 2)}</pre>);
+  if (data)
+    return (
+      <GithubUser
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
+      />
+    );
   return <h1>Data</h1>;
 }
 
